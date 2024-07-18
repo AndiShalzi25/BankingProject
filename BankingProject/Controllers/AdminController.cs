b2cde5561655ed981a3cb1227a0a54a34c3fd0e2
Andi <andishalzi25@gmail.com>
2024-07-18 19:58:35 +0200

    finish up CRUD logic for Account

--- BankingProject/Controllers/AdminController.cs
+++ BankingProject/Controllers/AdminController.cs
@@ -134,5 +134,112 @@ namespace BankingProject.Controllers
 
 			return View();
 		}
+
+
+		public async Task<IActionResult> CreateAccount(int userId, string accountName)
+		{
+
+			var user = await _context.Users.Where(u => u.Id == userId).FirstAsync();
+
+			var newAccount = new Account()
+			{
+				Name = accountName,
+				TotalBalance = 0,
+				UserId = user.Id,
+				User = user,
+				CreatedAt = DateTime.Now,
+				CreatedById = 1002
+			};
+
+			await _context.Accounts.AddAsync(newAccount);
+			await _context.SaveChangesAsync();
+
+			var newDebitCard = new DebitCard()
+			{
+				SerialNumber = GenerateRandomString(10, numberChars),
+				CardNumber = GenerateRandomString(16, numberChars),
+				CCV = GenerateRandomString(3, numberChars),
+				Name = $"{user.Name} {user.Surname}",
+				ExpireDate = newAccount.CreatedAt.AddYears(5),
+				AccountId = newAccount.Id,
+				Account = newAccount
+			};
+
+			await _context.DebitCards.AddAsync(newDebitCard);
+			await _context.SaveChangesAsync();
+
+			var depositCurrency = "ALL";
+			var newDeposit = new Deposit()
+			{
+				Currency = depositCurrency,
+				Name = $"{user.Name} {user.Surname} deposit {depositCurrency}",
+				IBAN = $"{GenerateRandomString(31, letterAndNumberChars)}{depositCurrency}",
+				SWIFT = "NCBTAAL",
+				Balance = 0,
+				AccountId = newAccount.Id,
+				Account = newAccount,
+				CreatedById = 1002,
+				CreatedAt = DateTime.Now
+			};
+
+			await _context.Deposits.AddAsync(newDeposit);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("UserIndex", new { id = userId });
+		}
+
+
+		public async Task<IActionResult> EditAccount(int accountId, string accountName)
+		{
+			var accountToEdit = await _context.Accounts.Where(a => a.Id == accountId).FirstAsync();
+			var userId = accountToEdit.UserId;
+			accountToEdit.Name = accountName;
+
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("UserIndex", new { id = userId });
+		}
+
+		public async Task<IActionResult> DeleteAccount(int accountId)
+		{
+			var deposits = await _context.Deposits.Where(d => d.AccountId == accountId).ToListAsync();
+
+			_context.Deposits.RemoveRange(deposits);
+			await _context.SaveChangesAsync();
+
+			var debitCards = await _context.DebitCards.Where(d => d.AccountId == accountId).ToListAsync();
+			_context.DebitCards.RemoveRange(debitCards);
+			await _context.SaveChangesAsync();
+
+			var accountToRemove = await _context.Accounts.Where(a => a.Id == accountId).FirstAsync();
+
+			var userId = accountToRemove.UserId;
+
+			_context.Accounts.Remove(accountToRemove);
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction("UserIndex", new { id = userId });
+		}
+
+		public async Task<IActionResult> AccountInfo(int accountId) 
+		{
+			var account = await _context.Accounts.Include(a => a.User).Where(a => a.Id == accountId).FirstAsync();
+			var deposits = await _context.Deposits.Where(d => d.AccountId == accountId).ToListAsync();
+			var debitCards = await _context.DebitCards.Where(d => d.AccountId == accountId).ToListAsync();
+
+			var userFullName = $"{account.User.Name} {account.User.Surname}";
+
+			var model = new AccountInfoViewModel()
+			{
+				Account = account,
+				DebitCards = debitCards,
+				Deposits = deposits,
+				UserFullName = userFullName
+			};
+
+			ViewBag.AccountInfo = model;
+
+			return View();
+		}
 	}
 }
---
+++ BankingProject/DTO/AccountInfoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BankingProject.Models;
+
+namespace BankingProject.DTO
+{
+	public class AccountInfoViewModel
+	{
+		public Account Account { get; set; }
+		public List<Deposit> Deposits { get; set; }
+		public List<DebitCard> DebitCards { get; set; }
+		public string UserFullName { get; set; }
+	}
+}
