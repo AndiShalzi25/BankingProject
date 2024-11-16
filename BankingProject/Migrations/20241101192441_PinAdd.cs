2bb46af1a71963757e75d06a69e4bc313c7feffe
ElisaRuci7 <elisa.ruci7@gmail.com>
2024-11-16 15:33:05 +0100

    add pin to debit card

---
+++ BankingProject/Migrations/20241101192441_PinAdd.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankingProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class PinAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PIN",
+                table: "DebitCards",
+                type: "nvarchar(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PIN",
+                table: "DebitCards");
+        }
+    }
+}
